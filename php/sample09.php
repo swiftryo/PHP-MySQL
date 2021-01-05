8a0d7a7a6f27e83409f21ac4f9128f26b328bee6
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-05 10:41:55 +0900

    add_21

---
+++ php/sample09.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<?php
+$fruits = [
+  'apple'=>'りんご', 
+  'grspe'=>'ぶどう', 
+  'lemon'=>'レモン',
+  'tomato'=>'とまと',
+  'peach'=>'もも'
+];
+
+foreach ($fruits as $en => $ja) {
+  print ($en . ':' . $ja . "\n");
+}
+
+?>
+</pre>
+</main>
+</body>    
+</html>
