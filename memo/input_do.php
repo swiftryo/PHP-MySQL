89716aba76c01f1618bf7f6872aa44f1f9782bd6
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-09 08:41:52 +0900

    add-61

--- memo/input_do.php
+++ memo/input_do.php
@@ -19,14 +19,16 @@
 <h2>Practice</h2>
 <pre>
 <!-- ここにプログラムを記述します -->
-<?php
+  <?php
     try {
-      $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root');  
-      $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
-    }catch(PDOException $e){
-     echo 'DB接続エラー:' . $e ->getMessage();
-    }
-    ?>
+      $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root'); 
+      $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+      $statement->execute(array($_POST['memo']));
+      echo 'メッセージが登録されました。';
+        }catch(PDOException $e){
+        echo 'DB接続エラー:' . $e ->getMessage();
+        }
+  ?>
 </pre>
 </main>
 </body>    
