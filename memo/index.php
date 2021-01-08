953b8e1505496a9ee63a632be108358590069d0c
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-08 15:59:27 +0900

    add-59

--- memo/index.php
+++ memo/index.php
@@ -29,8 +29,10 @@
     echo 'DB接続エラー: ' . $e->getMessage();
   }
 
-  $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰、ピンク、甘い"');
-  echo $count . '件のデータを挿入しました';
+  $records = $db->query('SELECT * FROM my_items');
+  while ($record = $records->fetch()) {
+    print($record['item_name'] . "\n");
+  }
 ?>
 </pre>
 </main>
