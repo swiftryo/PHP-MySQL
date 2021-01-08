d1928c834a208775fd9ff3b565f3ae3e2ad65c09
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-08 15:48:34 +0900

    add-58

--- memo/index.php
+++ memo/index.php
@@ -28,6 +28,9 @@
   } catch(PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
   }
+
+  $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰、ピンク、甘い"');
+  echo $count . '件のデータを挿入しました';
 ?>
 </pre>
 </main>
