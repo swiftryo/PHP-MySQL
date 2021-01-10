b428b2f4fa85beaf59705ff8684177842a133779
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-11 05:40:50 +0900

    add-75

--- mini_bbs/join/index.php
+++ mini_bbs/join/index.php
@@ -14,8 +14,20 @@
 		if ($_POST['password'] === '') {
 			$error['password'] = 'blank';
 		}
+		$fileName = $_FILES['image']['name'];
+		if (!empty($fileName)) {
+			$ext = substr($fileName, -3);
+			if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
+				$error['image'] = 'type';
+			}
+		}
+
+
 		if (empty($error)) {
+			$image = date('YmdHis') . $_FILES['image']['name'];
+			move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
 			$_SESSION['join'] = $_POST;
+			$_SESSION['join']['image'] = $image;
 			header('Location: check.php');
 			exit();
 		}
@@ -78,6 +90,9 @@
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
+					<?php if ($error['image'] === 'type'): ?>
+						<p class="error">*写真などは、[.gif][.png][.jpg]の画像を指定してください。</p>
+					<?php endif; ?>
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
