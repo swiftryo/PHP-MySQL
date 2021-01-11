59479fceeb6fd05661b4a983cde8ba885b8725fa
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-11 14:57:03 +0900

    add-80

--- mini_bbs/login.php
+++ mini_bbs/login.php
@@ -2,7 +2,12 @@
   session_start();
   require('dbconnect.php');
 
+  if ($_COOKIE['email'] !== '') {
+    $email = $_COOKIE['email'];
+  }
+
   if (!empty($_POST)) {
+    $email = $_POST['email'];
     if ($_POST['email'] !== '' && $_POST['password'] !== '') {
       $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
       $login->execute(array(
@@ -14,9 +19,17 @@
       if ($member) {
         $_SESSION['id'] = $member['id'];
         $_SESSION['time'] = time();
+
+        if ($_POST['save'] === 'on') {
+          setcookie('email', $_POST['email'], time()+60*60*24*14);
+        }
         header('Location: index.php');
         exit();
+      } else {
+        $error['login'] = 'failed';
       }
+    } else {
+      $error['login'] = 'blank';
     }
   }
 
@@ -46,11 +59,17 @@
       <dl>
         <dt>メールアドレス</dt>
         <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+          <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
+          <?php if ($error['login'] === 'blank'): ?>
+            <p class="error">*メールアドレスとパスワードを記入してください</p>
+          <?php endif ?>
+          <?php if ($error['login'] === 'failed'): ?>
+            <p class="error">*ログインに失敗しました。正しくご記入ください。</p>
+          <?php endif ?>
         </dd>
         <dt>パスワード</dt>
         <dd>
-          <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password']); ?>" />
+          <input type="password" name="password" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
         </dd>
         <dt>ログイン情報の記録</dt>
         <dd>
