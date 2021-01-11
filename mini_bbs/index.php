35afccc2670e10ca61ffd6546c05e36221500031
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-11 16:02:07 +0900

    add-85

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -15,10 +15,11 @@
 
   if (!empty($_POST)) {
     if ($_POST['message'] !== '') {
-      $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+      $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
       $message->execute(array(
         $member['id'],
-        $_POST['message']
+        $_POST['message'],
+        $_POST['reply_post_id']
       ));
 
       header('Location: index.php');
@@ -27,6 +28,14 @@
   }
 
   $posts = $db->query('SELECT m.name, m.picture, p.*FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+  if (isset($_REQUEST['res'])) {
+    // 返信の処理
+    $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+    $response->execute(array($_REQUEST['res']));
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table ['message'];
+  }
 ?>
 
 
@@ -55,8 +64,8 @@
           さん、メッセージをどうぞ
         </dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?> " />
         </dd>
       </dl>
       <div>
@@ -76,7 +85,7 @@
           （<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>
 ）
         </span>
-        [<a href="index.php?res=">Re</a>]
+        [<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]
       </p>
       <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?>
 </a>
