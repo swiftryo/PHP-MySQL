503388b21cb7a53ff4cda9b91e65199642589d97
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-18 10:24:58 +0900

    add39

--- form/input.php
+++ form/input.php
@@ -1,11 +1,13 @@
 <?php
+session_start();
 // クリックジャッキング
 header('X-FRAME-OPTIONS:DENY');
 
-  // if(!empty($_POST)){
-  //   var_dump($_POST);
-  // }
+  if(!empty($_POST)){
+    var_dump($_POST);
+  }
   // スーパーグローバル変数　php 9種類　連想配列になっている
+  // CSRF　偽物のinput.php->悪意のあるページ
 
   // 入力、確認、完了
 
@@ -37,6 +39,7 @@ header('X-FRAME-OPTIONS:DENY');
 
 
 <?php if($pageFlag === 1) : ?>
+<?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
   <form method="POST" action="input.php">
     氏名
     <?php echo h($_POST['your_name']); ?>
@@ -44,20 +47,59 @@ header('X-FRAME-OPTIONS:DENY');
     メールアドレス
     <?php echo h($_POST['email']); ?>
     <br>
+    ホームページ
+    <?php echo h($_POST['url']); ?>
+    <br>
+    性別
+    <?php
+      if($_POST['gender'] === '0'){ echo '男性';}
+      if($_POST['gender'] === '1'){ echo '女性';}
+    ?>
+    <br>
+    年齢
+    <?php
+      if($_POST['age'] === '1'){ echo '~19歳' ;}
+      if($_POST['age'] === '2'){ echo '20~29' ;}
+      if($_POST['age'] === '3'){ echo '30~39' ;}
+      if($_POST['age'] === '4'){ echo '40~49' ;}
+      if($_POST['age'] === '5'){ echo '50~59' ;}
+      if($_POST['age'] === '6'){ echo '60~' ;}
+    ?>
+    <br>
+    お問い合わせ内容
+    <?php echo h($_POST['contact']); ?>
+    <br>
     <input type="submit" name="back" value="戻る">
     <input type="submit" value="送信する" name="btn_submit">
     <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
     <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+    <input type="hidden" name="url" value="<?php echo h($_POST['url']); ?>">
+    <input type="hidden" name="gender" value="<?php echo h($_POST['gender']); ?>">
+    <input type="hidden" name="age" value="<?php echo h($_POST['age']); ?>">
+    <input type="hidden" name="contact" value="<?php echo h($_POST['contact']); ?>">
+    <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
   </form>
+<?php endif; ?>
 <?php endif; ?>
 
 
 <?php if($pageFlag === 2) : ?>
-  送信が完了しました。
+  <?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
+    送信が完了しました。
+    <?php unset($_SESSION['csrfToken']); ?>
+  <?php endif; ?>
 <?php endif; ?>
 
 
+
 <?php if($pageFlag === 0) : ?>
+<?php 
+  if(!isset($_SESSION['csrfToken'])) {
+    $csrfToken = bin2hex(random_bytes(32));
+    $_SESSION['csrfToken'] = $csrfToken;
+  }
+  $token = $_SESSION['csrfToken'];
+?>
   <form method="POST" action="input.php">
     氏名
     <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);} ?>">
@@ -65,7 +107,36 @@ header('X-FRAME-OPTIONS:DENY');
     メールアドレス
     <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);} ?>">
     <br>
+    
+    ホームページ
+    <input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']);} ?>">
+    <br>
+    性別
+    <input type="radio" name="gender" value="0"
+    <?php if(isset($_POST['gender']) && $_POST['gender'] === '0' ){ echo 'checked';} ?>>男性
+    <input type="radio" name="gender" value="1"
+    <?php if(!empty($_POST['gender']) && $_POST['gender'] === '1' ){ echo 'checked';} ?>>女性
+    <br>
+    年齢
+    <select name="age">
+      <option value="">選択してください</option>
+      <option value="1">~19</option>
+      <option value="2">20~29</option>
+      <option value="3">30~39</option>
+      <option value="4">40~49</option>
+      <option value="5">50~59</option>
+      <option value="6">60~</option>
+    </select>
+    <br>
+    お問い合わせ内容
+    <textarea name="contact">
+      <?php if(!empty($_POST['contact'])){echo h($_POST['contact']);} ?>
+    </textarea>
+    <br>
+    <input type="checkbox" name="caution" value="1"> 注目事項にチェックする
+    <br>
     <input type="submit" value="確認する" name="btn_confirm">
+    <input type="hidden" name="csrf" value="<?php echo $token; ?>" >
   </form>
 <?php endif; ?>
 
