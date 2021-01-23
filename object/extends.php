6adae1e6911d930c5b987a8322c6e96a84f3a077
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-23 16:53:34 +0900

    add-62

---
+++ object/extends.php
@@ -0,0 +1,54 @@
+<?php
+
+  class BaseProduct{
+    public function echoProduct(){
+      echo '親クラスです';
+    }
+
+  }
+  class Product extends BaseProduct {
+    // アクセス修飾子, private（外からアクセスできない）, protected（自分と継承したクラス）, public（公開）
+
+    // 変数
+    private $product = [];
+
+    // 関数
+    // 初回
+    function __construct($product){
+      $this->product = $product;
+    }
+
+    public function getProduct(){
+      echo $this->product;
+    }
+
+    public function addProduct($item){
+      $this->product .= $item;
+    }
+
+    public static function getStaticProduct($str){
+      echo $str;
+    }
+  }
+
+  $instance = new Product('テスト');
+
+  var_dump($instance);
+
+  $instance->getProduct();
+  echo '<br>';
+  
+  $instance->echoProduct();
+  echo '<br>';
+
+  $instance->addProduct('追加分');
+  echo '<br>';
+
+  $instance->getProduct();
+  echo '<br>';
+
+  Product::getStaticProduct('静的');
+  echo '<br>';
+
+?>
+
